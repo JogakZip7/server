ffaaaeea2ebaa4e61659a8e359d4da7c5a9a4112
kiminnaaa <bexemen@gmail.com>
2025-02-19 13:11:00 +0900

    [ADD] owner 추가

--- src/routes/group/createGroup.js
+++ src/routes/group/createGroup.js
@@ -7,17 +7,17 @@ module.exports = (db) => {
   // 그룹 생성 라우트
   router.post("/", auth, async (req, res) => {
     try {
-      const userId = req.user.id;
+      const ownerId = req.user.id;
       const { name, imageUrl, introduction } = req.body;
       if (!name || !imageUrl || !introduction) throw { status: 400, message: "잘못된 요청입니다" };
 
       const [result] = await db.execute(
-        "INSERT INTO `GROUP` (name, imageUrl, introduction, postCount, memberCount) VALUES (?, ?, ?, 0, 1)",
-        [name, imageUrl, introduction]
+        "INSERT INTO `GROUP` (owner, name, imageUrl, introduction, postCount, memberCount) VALUES (?, ?, ?, ?, 0, 1)",
+        [ownerId, name, imageUrl, introduction]
       );
 
       await db.execute("INSERT INTO PARTICIPATE (userId, groupId) VALUES (?, ?)", [userId, result.insertId]);
-      res.status(201).json({ id: result.insertId, name, imageUrl, introduction, postCount: 0, badges: [] });
+      res.status(201).json({ id: result.insertId, owner: ownerId, name, imageUrl, introduction, postCount: 0, badges: [] });
     } catch (err) {
       res.status(err.status || 500).json({ message: err.message || "서버 오류가 발생했습니다" });
     }
