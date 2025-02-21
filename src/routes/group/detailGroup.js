023d03649852678867ed3396aed09fad1453bea6
shinwon2001@naver.com <shinwon2001@naver.com>
2025-02-21 20:08:22 +0900

    Add function(readPost) to detailGroup.js

--- src/routes/group/detailGroup.js
+++ src/routes/group/detailGroup.js
@@ -8,11 +8,15 @@ module.exports = (db) => {
     try {
       const userId = req.user.id;
       const { groupId } = req.params;
+
+      //그룹 번호가 잘못됐거나 존재하지 않는 그룹인 경우
       if (!groupId) throw { status: 400, message: "잘못된 요청입니다" };
 
       const [groupRows] = await db.query("SELECT * FROM `GROUP` WHERE id = ?", [groupId]);
       if (!groupRows.length) throw { status: 404, message: "존재하지 않는 그룹입니다" };
 
+
+      //그룹 자체 정보를 받아오는 코드 시작 (게시글 제외)
       const { id, name, imageUrl, postCount, introduction, badges, memberCount } = groupRows[0];
 
       let existingBadges = [];
@@ -51,7 +55,127 @@ module.exports = (db) => {
         await db.execute("UPDATE `GROUP` SET badges = ? WHERE id = ?", [JSON.stringify(existingBadges), groupId]);
       }
 
-      res.status(200).json({ id, name, imageUrl, badges: existingBadges, postCount, introduction });
+
+      //그룹 내 게시글 관련 코드 시작.
+      //게시글 정렬 기준 받아옴
+      const {
+        page = 1,
+        pageSize = 8,
+        sortBy = "latest",
+      } = req.query;
+      
+      //게시글 관련 응답에 사용할 변수 선언
+      let result;
+
+      let totalItemCount;
+      let totalPages;
+      let offset;
+
+      // 테이블 정렬 기준 정하기
+      let sort = "";
+      switch (sortBy) {
+        case "latest":
+          sort = "P.createdAt";
+          break;
+        case "mostCommented":
+          sort = "P.commentCount";
+          break;
+        case "mostLiked":
+          sort = "P.likeCount";
+          break;
+        default:
+          sort = "P.createdAt";
+      }
+
+      
+      //유저가 그룹 내 사람인지 체크
+      const [participation] = await db.execute(
+        "SELECT * FROM PARTICIPATE WHERE userId = ? AND groupId = ?",
+        [userId, groupId]
+      );
+      
+      
+      //유저가 그룹 내 사람이 아닌 경우 공개게시글만 리턴
+      if (!participation.length) {
+        
+        //공개 게시글 총 페이지 수 및 총 게시글 수 카운트
+        const [countPost] = await db.query(`
+         SELECT COUNT(*) AS totalCount
+         FROM POST
+         WHERE groupId = ? AND isPublic = 1`,
+         [groupId]
+       );
+
+       totalItemCount = countPost[0].totalCount;
+       totalPages = Math.ceil(totalItemCount / pageSize);
+       offset = (page - 1) * (pageSize);
+
+      //POST 테이블 정렬 후 페이지에 따른 필터링
+      const [resultRow] = await db.query(
+        `
+          SELECT P.*, U.nickname
+          FROM POST P
+          JOIN USER U ON P.userId = U.id
+          WHERE P.groupId = ? AND P.isPublic = 1
+          ORDER BY ${db.escapeId(sort)} DESC
+          LIMIT ? OFFSET ?
+        `,
+        [groupId, parseInt(pageSize), parseInt(offset)]
+      );
+      result = resultRow;
+
+      } else{ //유저가 그룹 내 사람인 경우 (모든 그룹 게시물 리턴)
+        
+        //모든 게시글 총 페이지 수 및 총 게시글 수 카운트
+        const [countPost] = await db.query(`
+          SELECT COUNT(*) AS totalCount
+          FROM POST
+          WHERE groupId = ?`,
+          [groupId]
+        );
+ 
+        totalItemCount = countPost[0].totalCount;
+        totalPages = Math.ceil(totalItemCount / pageSize);
+        offset = (page - 1) * (pageSize);
+ 
+       //POST 테이블 정렬 후 페이지에 따른 필터링
+       const [resultRow] = await db.query(
+         `
+           SELECT P.*, U.nickname
+           FROM POST P
+           JOIN USER U ON P.userId = U.id
+           WHERE P.groupId = ?
+           ORDER BY ${db.escapeId(sort)} DESC
+           LIMIT ? OFFSET ?
+         `,
+         [groupId, parseInt(pageSize), parseInt(offset)]
+       );
+
+       result = resultRow;
+
+
+      }
+
+      //response 객체
+      const response = {
+        currentPage: page,
+        totalPages: totalPages,
+        totalItemCount: totalItemCount,
+        data: result.map((post) => ({
+          id: post.id,
+          nickname: post.nickname,
+          title: post.title,
+          imageUrl: post.imageUrl,
+          location: post.location,
+          moment: post.moment,
+          isPublic: post.isPublic,
+          likeCount: post.likeCount,
+          commentCount: post.commentCount,
+          createdAt: post.createdAt,
+        })),
+      };
+
+      res.status(200).json({ id, name, imageUrl, badges: existingBadges, postCount, introduction, response });
     } catch (err) {
       res.status(err.status || 500).json({ message: err.message || "서버 오류가 발생했습니다" });
     }
